d5499c5de84ff1bef064e919b088c476022c4de4
Asraful <asrafulislamcodes@gmail.com>
2023-03-06 19:31:32 +0600

    o

--- app.js
+++ app.js
@@ -36,12 +36,12 @@ app.use((req, res, next) => {
 });
 
 // Serve static files from React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
-// Serve the React app for all other requests
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// // Serve the React app for all other requests
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 // Middleware to handle errors
 app.use(errorHandler);
