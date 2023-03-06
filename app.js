999a4d69e0c39cf5bc199b93d2dbcad576ea2b43
Asraful <asrafulislamcodes@gmail.com>
2023-03-06 18:43:00 +0600

    fixed cors

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ app.use(fileUpload());
 
 const user = require('./routes/userRoutes');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://testing-orcin-omega.vercel.app'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/api/v1', user);
 
 // middle ware
