40332c1c8afae8dd6c50aa9c0b340fca3143f007
Asraful <asrafulislamcodes@gmail.com>
2023-03-06 18:37:22 +0600

    added https

--- app.js
+++ app.js
@@ -13,15 +13,9 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-// Route imports
-
-// const product = require('./routes/productRoute');
 const user = require('./routes/userRoutes');
-// const order = require('./routes/orderRoute');
 
-// app.use('/api/v1', product);
 app.use('/api/v1', user);
-// app.use('/api/v1', order);
 
 // middle ware
 app.use(errorHandler);
