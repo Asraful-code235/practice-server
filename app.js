a5a54b5e177631b695e6b88e05a3c71cb81c2ed8
Asraful <asrafulislamcodes@gmail.com>
2023-03-06 19:00:39 +0600

    o

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({
+    path: path.join(__dirname, '/config/config.env'),
+  });
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -13,22 +20,30 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-const user = require('./routes/userRoutes');
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/v1', userRoutes);
 
+// Set headers to allow cross-origin requests
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
-  //   res.header(
-  //     'Access-Control-Allow-Origin',
-  //     'https://testing-orcin-omega.vercel.app'
-  //   );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, OPTIONS, PATCH, DELETE, POST, PUT'
+  );
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
-app.use('/api/v1', user);
+// Serve static files from React app
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-// middle ware
+// Serve the React app for all other requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// Middleware to handle errors
 app.use(errorHandler);
 
 module.exports = app;
